826504fc25a4e241f24cb55aa617aa16fe0f7d8a
Bujar <begisholli.bujar@gmail.com>
2017-12-31 13:35:38 +0100

    bug fix

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -45,6 +45,18 @@
         #myCarousel{
           max-height: 520px;
         }
+
+        .carousel-control-prev-icon{
+          background-color: #222428;
+          color: black;
+          height: 100px;
+        }
+
+        .carousel-control-next-icon{
+          background-color: #222428;
+          color: black;
+          height: 100px;
+        }
     </style>
 @endsection
 
@@ -142,7 +154,7 @@
             </div>
             <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span class="sr-only" >Previous</span>
             </a>
             <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
